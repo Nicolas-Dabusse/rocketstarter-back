ae080802423771b8372bc1b8b82cd7e1e968c927
Nicolas-Dabusse <nico43d@hotmail.fr>
2025-12-23 12:03:28 +0100

    add projects module

--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { ProjectsService } from './projects.service';
 import { ProjectsController } from './projects.controller';
+import { Project } from '../models/Project';
+import { Category } from '../models/Category';
+import { ProjectCategory } from '../models/ProjectCategory';
 
 @Module({
+  imports: [
+    // Enregistrer les modèles pour l'injection
+    SequelizeModule.forFeature([Project, Category, ProjectCategory]),
+  ],
   providers: [ProjectsService],
-  controllers: [ProjectsController]
+  controllers: [ProjectsController],
+  exports: [ProjectsService], // Exporter pour utiliser dans d'autres modules
 })
 export class ProjectsModule {}
